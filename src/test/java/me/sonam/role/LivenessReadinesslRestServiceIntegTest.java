1baa7a023653dbe728dc2df343f3ff4554638c6b
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2023-10-01 19:19:38 -0600

    fix test

--- src/test/java/me/sonam/role/LivenessReadinesslRestServiceIntegTest.java
+++ src/test/java/me/sonam/role/LivenessReadinesslRestServiceIntegTest.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -19,6 +21,9 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @SpringBootTest(classes = {SpringApplication.class})
 public class LivenessReadinesslRestServiceIntegTest {
   private static final Logger LOG = LoggerFactory.getLogger(LivenessReadinesslRestServiceIntegTest.class);
+
+  @MockBean
+  private ReactiveJwtDecoder reactiveJwtDecoder;
 
   @Autowired
   private WebTestClient client;
