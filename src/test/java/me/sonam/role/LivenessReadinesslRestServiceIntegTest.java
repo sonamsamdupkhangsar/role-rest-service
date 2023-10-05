29c3dbe4f6ed8ae6eec3b221f1d5b0e8b8423af3
Sonam Samdupkhangsar <sonamhava@gmail.com>
2023-10-05 09:33:38 -0600

    Feature/k8 (#2)
    * update
    
    * fix test
    
    ---------
    
    Co-authored-by: Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>

--- src/test/java/me/sonam/role/LivenessReadinesslRestServiceIntegTest.java
+++ src/test/java/me/sonam/role/LivenessReadinesslRestServiceIntegTest.java
@@ -1,12 +1,15 @@
 package me.sonam.role;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -18,6 +21,9 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @SpringBootTest(classes = {SpringApplication.class})
 public class LivenessReadinesslRestServiceIntegTest {
   private static final Logger LOG = LoggerFactory.getLogger(LivenessReadinesslRestServiceIntegTest.class);
+
+  @MockBean
+  private ReactiveJwtDecoder reactiveJwtDecoder;
 
   @Autowired
   private WebTestClient client;
