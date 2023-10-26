2139c1a37c302c5b5c17e15811ca60a8b8b4078a
Sonam Samdupkhangsar <sonamhava@gmail.com>
2023-10-26 17:58:10 -0600

    Feature/fix (#4)
    * use name for query param
    
    * remove commented out code
    
    ---------
    
    Co-authored-by: Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>

--- src/main/java/me/sonam/role/repo/RoleRepository.java
+++ src/main/java/me/sonam/role/repo/RoleRepository.java
@@ -11,26 +11,26 @@ import java.util.UUID;
 
 public interface RoleRepository extends ReactiveCrudRepository<Role, UUID> {
     @Query("select r.* from Role r,  Role_Organization ro " +
-            "where ro.organization_id = ?1  " +
-            " and r.name = ?2 " +
+            "where ro.organization_id = :id " +
+            " and r.name = :name " +
             " and ro.role_id=r.id")
     Flux<Role> findByOrganizationIdAndName(@Param("id") UUID id, @Param("name") String name);
 
     @Query("select r.* from Role r,  Role_User ru " +
-            "where ru.user_id = ?1  " +
-            " and r.name = ?2 " +
+            "where ru.user_id = :userId " +
+            " and r.name = :name " +
             " and ru.role_id=r.id")
     Flux<Role> findByUserIdAndName(@Param("userId") UUID userId, @Param("name") String name);
 
 
     @Query("select r.* from Role r,  Role_Organization ro " +
-            "where ro.organization_id = ?1  " +
+            "where ro.organization_id = :organizationId " +
             " and ro.role_id=r.id")
     Flux<Role> findByOrganizationId(@Param("organizationId") UUID organizationId, Pageable pageable);
 
 
     @Query("select r.* from Role r,  Role_User ru " +
-            "where ru.user_id = ?1  " +
+            "where ru.user_id = :userId " +
             " and ru.role_id=r.id")
     Flux<Role> findByUserId(@Param("userId") UUID userId, Pageable pageable);
 
