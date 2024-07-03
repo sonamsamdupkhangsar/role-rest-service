d74f1eb7be28605a01664fc4e3b0d83df2c4b4f4
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2024-07-03 13:31:10 -0600

    remove drop table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,4 @@
 create table if not exists Role(id uuid primary key, name varchar, user_id uuid);
-drop table if exists Client_User_Role;
 create table if not exists Client_User_Role(id uuid primary key, client_id uuid, user_id uuid, role_id uuid);
 create table if not exists Role_Organization(id uuid primary key, role_id uuid, organization_id uuid);
 create table if not exists Role_User(id uuid primary key, role_id uuid, user_id uuid);
